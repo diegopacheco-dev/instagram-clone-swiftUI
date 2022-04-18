e592386196c11c24fd8b062cc81f9756b6e5ab2a
Diego <ericksonder017@gmail.com>
2022-04-18 17:13:51 -0500

    Starting project

---
+++ Instagram-clone-swiftUI/Instagram_clone_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_clone_swiftUIApp.swift
+//  Instagram-clone-swiftUI
+//
+//  Created by Diego on 18/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_clone_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Instagram-clone-swiftUI/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instagram-clone-swiftUI
+//
+//  Created by Diego on 18/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
